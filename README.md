32f6f92a87a8c7a0d2dac2bcf49aad578a9cb985
DocDoWell <dandyowl84@gmail.com>
2024-06-26 12:19:59 +0100

    first commit - background image not showing on about page

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GolangWebApp
 A simple WebApp in Golang
+
+Uses Chi as routing package.
+
+
+go run ./cmd/web -cache=true to use cache otherwise go run ./cmd/web
+
+
+WebP for images - has good support.
---
+++ cmd/web/routes.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux:= chi.NewRouter()
+
+	//keep app up and running
+	mux.Use(middleware.Recoverer)
+	//timeout after 1 min
+	mux.Use(middleware.Timeout(60*time.Second))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	mux.Get("/", app.ShowHome)
+	mux.Get("/{page}", app.ShowPage)
+
+	return mux
+}
